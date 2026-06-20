f4c985a0390bb907c30a9a4e6735b76fc3793f0d
yehoshua <y0587828030@gmail.com>
2026-06-20 22:29:47 +0300

    stap_6_Operators_bool

--- logic.py
+++ logic.py
@@ -1,20 +1,30 @@
-#stap 1
-is_online = True
-has_access = False
-print(is_online and has_access)
+# #stap 1
+# is_online = True
+# has_access = False
+# print(is_online and has_access)
 
-#stap2
-print(is_online or has_access)
+# #stap2
+# print(is_online or has_access)
 
-#stap3 
-status = False
-print(not(status))
+# #stap3 
+# status = False
+# print(not(status))
 
-#stap 4
-age = 20
-id = True
-print(age>18 and id)
+# #stap 4
+# age = 20
+# id = True
+# print(age>18 and id)
 
-#stap 5
-level = 3
-print(1<level and 5>level)
+# #stap 5
+# level = 3
+# print(1<level and 5>level)
+
+#stap 6
+a = 0
+b = "hello"
+c = ""
+
+print(bool(a))
+print(bool(b))
+print(bool(c))
+
