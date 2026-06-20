df5d42a899c38ebaccc5e71c05e97a49aaa94529
yehoshua <y0587828030@gmail.com>
2026-06-20 23:09:48 +0300

    stap_8_Operators_bool

--- logic.py
+++ logic.py
@@ -1,40 +1,40 @@
-# #stap 1
-# is_online = True
-# has_access = False
-# print(is_online and has_access)
+#stap 1
+is_online = True
+has_access = False
+print(is_online and has_access)
 
-# #stap2
-# print(is_online or has_access)
+#stap2
+print(is_online or has_access)
 
-# #stap3 
-# status = False
-# print(not(status))
+#stap3 
+status = False
+print(not(status))
 
-# #stap 4
-# age = 20
-# id = True
-# print(age>18 and id)
+#stap 4
+age = 20
+id = True
+print(age>18 and id)
 
-# #stap 5
-# level = 3
-# print(1<level and 5>level)
+#stap 5
+level = 3
+print(1<level and 5>level)
 
-# #stap 6
-# a = 0
-# b = "hello"
-# c = ""
+#stap 6
+a = 0
+b = "hello"
+c = ""
 
-# print(bool(a))
-# print(bool(b))
-# print(bool(c))
-
-# #stap 7
-# x = None
-# y = 42
-
-# print(x or y)
+print(bool(a))
+print(bool(b))
+print(bool(c))
 
 #stap 7
+x = None
+y = 42
+
+print(x or y)
+
+#stap 8
 username = "" 
 dafault = "guest"
 print(username or dafault)
